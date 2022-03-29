285dfa81ba81b634219cd365e22b8218bf2b0103
Baishali Dutta <me@itsbaishali.com>
2022-03-29 08:30:54 +0200

    Formatted ShallowNET

--- lib/nn/conv/shallownet.py
+++ lib/nn/conv/shallownet.py
@@ -1,32 +1,30 @@
 # import the necessary packages
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
 from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Conv2D, Activation, Flatten, Dense
+from tensorflow.keras.models import Sequential
+
 
 class ShallowNet:
-	@staticmethod
-	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last"
-		model = Sequential()
-		inputShape = (height, width, depth)
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last"
+        model = Sequential()
+        inputShape = (height, width, depth)
 
-		# if we are using "channels first", update the input shape
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
 
-		# define the first (and only) CONV => RELU layer
-		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
+        # define the first (and only) CONV => RELU layer
+        model.add(Conv2D(32, (3, 3), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation("relu"))
 
-		# softmax classifier
-		model.add(Flatten())
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # softmax classifier
+        model.add(Flatten())
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
 
-		# return the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
