16f2a67138844ef933cd0bea8f2f89ffd06f5322
Baishali Dutta <me@itsbaishali.com>
2022-04-02 08:33:28 +0200

    Added missing copyright info to __init__.py

--- lib/nn/conv/__init__.py
+++ lib/nn/conv/__init__.py
@@ -1,4 +1,9 @@
-# import the necessary packages
-from .shallownet import ShallowNet
+__author__ = "Baishali Dutta"
+__copyright__ = "Copyright (C) 2022 Baishali Dutta"
+__license__ = "Apache License 2.0"
+__version__ = "0.1"
+
 from .lenet import LeNet
 from .minivggnet import MiniVGGNet
+# import the necessary packages
+from .shallownet import ShallowNet
